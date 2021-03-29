c98b4f5e3afa6d568a73734e1709faf3fb3c7c5d
lx kish <urban.ktulhu@gmail.com>
2021-03-30 10:29:46 +1300

    Updates

---
+++ precache-manifest.123b85acc5c6b28e761d21dd0fd2b39c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a63d1786abdf997ceb736548ff542b",
+    "url": "/rithmetica/index.html"
+  },
+  {
+    "revision": "2df2d9bf31b3599cedd7",
+    "url": "/rithmetica/static/css/main.9ee447a9.chunk.css"
+  },
+  {
+    "revision": "c303276e92e56cc2a5ff",
+    "url": "/rithmetica/static/js/2.3d19d702.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rithmetica/static/js/2.3d19d702.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2df2d9bf31b3599cedd7",
+    "url": "/rithmetica/static/js/main.0558c7f9.chunk.js"
+  },
+  {
+    "revision": "150d3e4eb9a55edad056",
+    "url": "/rithmetica/static/js/runtime-main.9fb88564.js"
+  },
+  {
+    "revision": "f92264abf7168c9f41a57c09f262c7e8",
+    "url": "/rithmetica/static/media/shcema-big-screen-arrows.f92264ab.png"
+  },
+  {
+    "revision": "356deef3b8cd1ca823eafd2cde90e228",
+    "url": "/rithmetica/static/media/shcema-small-screen-arrows.356deef3.png"
+  }
+]);
