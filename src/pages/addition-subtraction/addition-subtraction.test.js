6911eae5ef8a0dfe3b84daab770081221fc43beb
lx kish <urban.ktulhu@gmail.com>
2021-04-18 23:59:48 +1200

    test(core): add test to Addition and Subtraction component

--- src/pages/addition-subtraction/addition-subtraction.test.js
+++ src/pages/addition-subtraction/addition-subtraction.test.js
@@ -1,96 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { render, fireEvent, cleanup, screen } from '@testing-library/react';
+import { render, fireEvent, cleanup } from '@testing-library/react';
+// import { render, fireEvent, cleanup, screen } from '@testing-library/react';
 // import { shallow, render, mount } from 'enzyme';
 
-import Header from './header.component';
+// import Header from './header.component';
+import AdditionSubtraction from './addition-subtraction.page';
 
-import appMenu from '../navigation/appMenu';
-import delimeter from '../navigation/delimeter';
-import siteMenu from '../navigation/siteMenu';
 
-import NavigationBar from '../navigation/navigation-bar/navigation-bar.component';
-import BurgerIcon from '../navigation/burger icon/icon-burger.component';
-import SlideBar from '../navigation/slide bar/slide-bar.component';
+// import appMenu from '../navigation/appMenu';
+// import delimeter from '../navigation/delimeter';
+// import siteMenu from '../navigation/siteMenu';
 
-describe('Header component rendering test suit', () => {
+// import NavigationBar from '../navigation/navigation-bar/navigation-bar.component';
+// import BurgerIcon from '../navigation/burger icon/icon-burger.component';
+// import SlideBar from '../navigation/slide bar/slide-bar.component';
+
+describe('Addition and Subtraction component test suit', () => {
 
     afterEach(cleanup);
 
-    it('button should contain link to "/logout" if user ID HAS passed to props', () => {
-        const props = {
-            data: {
-                _id: '123456789'
-            }
-        };
+    it('should add/remove css class "is-active" to the burger button on every click on it', () => {
 
         const { container } = render(
             <BrowserRouter>
-                <Header {...props} />
-            </BrowserRouter>
-        );
-
-        const button = container.querySelector('.btn--header');
-
-        expect(button).toHaveAttribute('href', '/logout');
-
-    })
-
-    it('button should contain link to "/signin" if user ID has NOT passed to props', () => {
-
-        const { container } = render(
-            <BrowserRouter>
-                <Header />
-            </BrowserRouter>
-        );
-
-        const button = container.querySelector('.btn--header');
-
-        expect(button).toHaveAttribute('href', '/signin');
-
-    })
-
-    it('should contain 15 links across component if user ID HAS passed to props', () => {
-        const props = {
-            data: {
-                _id: '123456789'
-            }
-        };
-
-        const { container } = render(
-            <BrowserRouter>
-                <Header {...props} />
-            </BrowserRouter>
-        );
-
-        // const button = container.querySelector('.btn--header');
-
-        expect(screen.getAllByRole('link').length).toBe(15);
-        // expect(screen.getAllByRole('link')).toHaveAttribute('href', '/profile');
-
-    })
-
-    it('should contain 10 links across component if user ID has NOT passed to props', () => {
-
-        const { container } = render(
-            <BrowserRouter>
-                <Header />
-            </BrowserRouter>
-        );
-
-        // const button = container.querySelector('.btn--header');
-
-        expect(screen.getAllByRole('link').length).toBe(10);
-        // expect(screen.getAllByRole('link')).toHaveAttribute('href', '/profile');
-
-    })
-
-    it('should change state on every click on BurgerIcon element', () => {
-
-        const { container } = render(
-            <BrowserRouter>
-                <Header />
+                <AdditionSubtraction />
             </BrowserRouter>
         );
 
@@ -107,11 +42,11 @@ describe('Header component rendering test suit', () => {
         expect(button.classList.contains('is-active')).toBe(false);
     })
 
-    it('should show and hide SlideBar element on click on BurgerIcon element', () => {
+    it('should add/remove class "is-active" to the slide bar on every click on the burger button', () => {
 
         const { container } = render(
             <BrowserRouter>
-                <Header />
+                <AdditionSubtraction />
             </BrowserRouter>
         );
 
@@ -129,6 +64,71 @@ describe('Header component rendering test suit', () => {
 
         expect(slidebar.classList.contains('is-active')).toBe(false);
     })
+
+    it('should contain 2 links in slider bar component', () => {
+
+        const { container } = render(
+            <BrowserRouter>
+                <AdditionSubtraction />
+            </BrowserRouter>
+        );
+
+        const slidebar = container.querySelector('.slide-bar');
+
+        // expect(slidebar.getAllByRole('link').length).toBe(2);
+        expect(slidebar.getAllByRole('link').length).toBe(2);
+        // expect(screen.getAllByRole('link')).toHaveAttribute('href', '/profile');
+
+    })
+
+    // it('should contain 10 links across component if user ID has NOT passed to props', () => {
+
+    //     const { container } = render(
+    //         <BrowserRouter>
+    //             <Header />
+    //         </BrowserRouter>
+    //     );
+
+    //     // const button = container.querySelector('.btn--header');
+
+    //     expect(screen.getAllByRole('link').length).toBe(10);
+    //     // expect(screen.getAllByRole('link')).toHaveAttribute('href', '/profile');
+
+    // })
+
+
+    // it('button should contain link to "/logout" if user ID HAS passed to props', () => {
+    //     const props = {
+    //         data: {
+    //             _id: '123456789'
+    //         }
+    //     };
+
+    //     const { container } = render(
+    //         <BrowserRouter>
+    //             <Header {...props} />
+    //         </BrowserRouter>
+    //     );
+
+    //     const button = container.querySelector('.btn--header');
+
+    //     expect(button).toHaveAttribute('href', '/logout');
+
+    // })
+
+    // it('button should contain link to "/signin" if user ID has NOT passed to props', () => {
+
+    //     const { container } = render(
+    //         <BrowserRouter>
+    //             <Header />
+    //         </BrowserRouter>
+    //     );
+
+    //     const button = container.querySelector('.btn--header');
+
+    //     expect(button).toHaveAttribute('href', '/signin');
+
+    // })
 })
 
 
