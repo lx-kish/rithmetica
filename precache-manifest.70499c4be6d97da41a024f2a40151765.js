7c83296f99c8be221117748423c0c59cb2288ea3
lx kish <urban.ktulhu@gmail.com>
2022-11-27 22:35:03 +1300

    Updates

---
+++ precache-manifest.70499c4be6d97da41a024f2a40151765.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b4509f71bf6847d0875530c3fda94c4",
+    "url": "/rithmetica/index.html"
+  },
+  {
+    "revision": "eb0102218f3ac8a15d7f",
+    "url": "/rithmetica/static/css/main.99f8b33b.chunk.css"
+  },
+  {
+    "revision": "eddc1224bb082eebcc73",
+    "url": "/rithmetica/static/js/2.075f645d.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/rithmetica/static/js/2.075f645d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb0102218f3ac8a15d7f",
+    "url": "/rithmetica/static/js/main.9b207321.chunk.js"
+  },
+  {
+    "revision": "68f4c3564b175faf2e39",
+    "url": "/rithmetica/static/js/runtime-main.6b6d6332.js"
+  },
+  {
+    "revision": "d5c49324692faeea2e1d79d5aaaf5ca5",
+    "url": "/rithmetica/static/media/Lemonada-Bold.d5c49324.ttf"
+  },
+  {
+    "revision": "2751630549691ec5d3ca917cd6be3b5d",
+    "url": "/rithmetica/static/media/Lemonada-Light.27516305.ttf"
+  },
+  {
+    "revision": "7a3f663c996eaa06f779b6c87a145f51",
+    "url": "/rithmetica/static/media/Lemonada-Medium.7a3f663c.ttf"
+  },
+  {
+    "revision": "5941e561ddc28759f9a6f71927e70841",
+    "url": "/rithmetica/static/media/Lemonada-Regular.5941e561.ttf"
+  },
+  {
+    "revision": "db980608ec40391cfed964340db01712",
+    "url": "/rithmetica/static/media/Lemonada-SemiBold.db980608.ttf"
+  },
+  {
+    "revision": "8821d472e8a08ec5239a7fd57fefda9a",
+    "url": "/rithmetica/static/media/schema-arrows--small.8821d472.png"
+  },
+  {
+    "revision": "dcfc6e73b559e808dce1b9e4b59fc5cc",
+    "url": "/rithmetica/static/media/shcema-arrows--big.dcfc6e73.png"
+  }
+]);
