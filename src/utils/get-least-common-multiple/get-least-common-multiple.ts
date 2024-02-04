9ba4441b87f8106bf9a2bb597610129d41b1afb6
lx-kish <urban.ktulhu@egmail.com>
2024-02-04 23:36:00 +1300

    test(core): add unit tests for getLeastCommonMultiple and handleKeyDown components

--- src/utils/get-least-common-multiple/get-least-common-multiple.ts
+++ src/utils/get-least-common-multiple/get-least-common-multiple.ts
@@ -1,22 +1,25 @@
 import getGreatestCommonDivisor from "../get-greatest-common-divisor/get-greatest-common-divisor";
+import isNumeric from "../is-numeric/is-numeric";
 
 /**
  * Finds least common multiple
  * Based on the https://stackoverflow.com/questions/47047682/least-common-multiple-of-an-array-values-using-euclidean-algorithm/49722579#49722579
  * @param  {number} a first number
- * 
+ *
  * @param  {number} b second number
- * 
+ *
  * @return {number} greatest common divisor
  */
 const getLeastCommonMultiple = (a: number, b: number): number => {
-  
   let leastCommonMultiple = 0;
 
   try {
-    leastCommonMultiple = a * b / getGreatestCommonDivisor(a, b);
-  }
-  catch (e) {
+    // to have greatest common divisor both values should be numeric
+    if (!isNumeric(a)) throw new Error(`Found non-numeric value: ${a}`);
+    if (!isNumeric(b)) throw new Error(`Found non-numeric value: ${b}`);
+
+    leastCommonMultiple = (a * b) / getGreatestCommonDivisor(a, b);
+  } catch (e) {
     if (e instanceof Error) {
       throw new Error(e.message);
     } else if (typeof e === "string") {
