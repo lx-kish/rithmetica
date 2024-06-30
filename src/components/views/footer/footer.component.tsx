e539b0d113a3fb8e0d447319a6ef23b8b5840780
lx-kish <urban.ktulhu@gmail.com>
2024-06-30 17:46:50 +1200

    fix(core): fix loosing focus after entering values and removing zero values of quantity field of settings block
    - add component state of the quantity component of settings compound component;
    
    - change redux settings quantity value on blur event instead of on change event;
    
    - change quantity field value type to a string instead of number to prevent entering zeros on empty fields.

--- src/components/views/footer/footer.component.tsx
+++ src/components/views/footer/footer.component.tsx
@@ -6,7 +6,7 @@ import IconLxKish from "../../icons-svg/icon-lx-kish.component";
 function Footer(): ReactElement {
   return (
     <footer className="footer">
-      <p className="footer__version">v9.6.0</p>
+      <p className="footer__version">v9.6.1</p>
       <p className="footer__credentials">
         &#169; 2024. Built with
         <span className="footer__icon-box">
