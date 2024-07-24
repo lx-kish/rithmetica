25cf3d869b0647c709428bb3afab9ec9806920e1
lx-kish <urban.ktulhu@gmail.com>
2024-07-24 21:47:49 +1200

    refactor(core): refactor get-rounded-to-fixed.ts utility function
    - adjust function signature and export for better readability;
    
    - make precision an absolute value to avoid getting errors of using negative precision values;
    
    - add try-catch block to intercept possible errors;
    
    - adjust function call and signature at one-digit-decimals-add-subtract.ts operands generator;
    
    - adjust function call and signature at tenth-add-subtract.ts operands generator;
    
    - adjust function call and signature at two-digit-decimals-add-subtract.ts.

--- src/utils/get-rounded-to-fixed/get-rounded-to-fixed.ts
+++ src/utils/get-rounded-to-fixed/get-rounded-to-fixed.ts
@@ -1,9 +1,32 @@
-export function getRoundedToFixed(
-  input: string | number,
-  digits: number
-): number {
-  const rounder = Math.pow(10, digits);
-  return Number(
-    (Math.round(Number(input) * rounder) / rounder).toFixed(digits)
-  );
+/**
+ * detailed explanation: https://stackoverflow.com/questions/7342957/how-do-you-round-to-one-decimal-place-in-javascript/12698296#12698296
+ *
+ * @param input string or number floating value to round
+ * @param precision number of decimal part to round up to
+ * @returns
+ */
+
+function getRoundedToFixed(input: string | number, precision: number): number {
+  let roundedToFixed = -1;
+  try {
+    if (!input) throw new Error("Input has not been provided!");
+    if (!precision) throw new Error("Precision has not been provided!");
+
+    const absPrecision = Math.abs(precision); // only non-negative number is allowed
+
+    const rounder = Math.pow(10, absPrecision);
+    roundedToFixed = Number(
+      (Math.round(Number(input) * rounder) / rounder).toFixed(absPrecision)
+    );
+  } catch (e) {
+    if (e instanceof Error) {
+      throw new Error(e.message);
+    } else if (typeof e === "string") {
+      throw new Error(e);
+    }
+  }
+
+  return roundedToFixed;
 }
+
+export default getRoundedToFixed;
