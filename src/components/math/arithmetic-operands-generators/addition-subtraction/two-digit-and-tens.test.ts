c24e7ba485ae9832757b1bcc618648c38a1f50bb
lx-kish <urban.ktulhu@gmail.com>
2024-08-16 20:56:50 +1200

    test(core): add unit tests for two-digit-and-tens.ts operands generator

---
+++ src/components/math/arithmetic-operands-generators/addition-subtraction/two-digit-and-tens.test.ts
@@ -0,0 +1,68 @@
+import twoDigitAndTens from "./two-digit-and-tens";
+
+import { operations } from "../../../../TS/constatnts/constants";
+
+describe("arythmetic - addition/subtraction - two-digit and tens test suit", () => {
+  const numberOfOperands = 2;
+
+  it("should return array", () => {
+    const result = twoDigitAndTens(operations.addition, numberOfOperands);
+
+    expect(Array.isArray(result)).toBeTruthy();
+  });
+
+  it("should return array with the length + 1 of the second parameter passed", () => {
+    const numberOfOperands = 3;
+    const result = twoDigitAndTens(operations.addition, numberOfOperands);
+
+    expect(result.length).toEqual(numberOfOperands + 1);
+  });
+
+  it("should return array containing only numbers", () => {
+    const result = twoDigitAndTens(operations.addition, numberOfOperands);
+
+    expect(result.some((item) => typeof item !== "number")).toBeFalsy();
+  });
+
+  it("should return array where the last value is the sum of previous values if the first parameter is '+'", () => {
+    const result = twoDigitAndTens(operations.addition, numberOfOperands);
+
+    const sum = result
+      .slice(0, result.length - 1)
+      .reduce((acc, current) => acc + current);
+
+    expect(sum).toEqual(result[result.length - 1]);
+  });
+
+  it("should return array where the first value is the sum of all the next values if the first parameter is '-'", () => {
+    const result = twoDigitAndTens(operations.subtraction, numberOfOperands);
+
+    const sum = result
+      .slice(1, result.length)
+      .reduce((acc, current) => acc + current);
+
+    expect(sum).toEqual(result[0]);
+  });
+
+  it("should return an empty array if the first parameter is NOT '+' or '-'", () => {
+    const result = twoDigitAndTens("", numberOfOperands);
+
+    expect(result.length).toEqual(0);
+  });
+
+  it("should return an empty array if the second parameter is less than 2", () => {
+    const numberOfOperands = 1;
+    const result = twoDigitAndTens(operations.addition, numberOfOperands);
+
+    expect(result.length).toEqual(0);
+  });
+
+  it("should return an empty array if the second parameter is null", () => {
+    const numberOfOperands = null;
+
+    // @ts-ignore
+    const result = twoDigitAndTens(operations.addition, numberOfOperands);
+
+    expect(result.length).toEqual(0);
+  });
+});
