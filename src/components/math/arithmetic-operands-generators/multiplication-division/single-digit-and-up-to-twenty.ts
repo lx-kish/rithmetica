e6dcd39a60a1b0fc348358a289f1491850135c3d
lx-kish <urban.ktulhu@gmail.com>
2024-08-26 22:18:06 +1200

    refactor(core): add input parameters checking and finally block to single-digit-and-up-to-twenty.ts operands generator

--- src/components/math/arithmetic-operands-generators/multiplication-division/single-digit-and-up-to-twenty.ts
+++ src/components/math/arithmetic-operands-generators/multiplication-division/single-digit-and-up-to-twenty.ts
@@ -1,5 +1,7 @@
 import randomInteger from "../../randoms/get-random-integer-in-a-range";
 
+import { operations } from "../../../../TS/constatnts/constants";
+
 /**
  *
  */
@@ -10,6 +12,19 @@ const singleDigitAndUpToTwenty = (
   const operands: number[] = [];
 
   try {
+    if (
+      operation !== operations.multiplication &&
+      operation !== operations.division
+    )
+      throw new Error(
+        `Unsupported operation ${operation} passed to the "Single digit and up to twenty" generator!`
+      );
+
+    if (typeof numberOfOperands !== "number" || numberOfOperands !== 2)
+      throw new Error(
+        `Not applicable number of operands ${numberOfOperands} has been passed to the "Single digit and up to twenty" generator!`
+      );
+
     // 1. Generate problem maximum with limits min=0+numberOfOperands, max=10
     let operand = 0;
     let product = 0;
@@ -26,14 +41,14 @@ const singleDigitAndUpToTwenty = (
 
     // 5. Push the problem maximum value to the appropriate place depend on operation (addition/subtraction)
     operands.splice(operation === "×" ? operands.length : 0, 0, product);
-
-    return operands;
   } catch (e) {
     if (e instanceof Error) {
       throw new Error(e.message);
     } else if (typeof e === "string") {
       throw new Error(e);
     }
+  } finally {
+    return operands;
   }
 };
 
