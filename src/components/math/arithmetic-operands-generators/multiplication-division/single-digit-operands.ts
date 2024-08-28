2ac84e7408177bcb86ecb05c7ef52cf7a2671615
lx-kish <urban.ktulhu@gmail.com>
2024-08-28 21:34:22 +1200

    refactor(core): add input parameters checking and finally block to single-digit-operands.ts operands generator

--- src/components/math/arithmetic-operands-generators/multiplication-division/single-digit-operands.ts
+++ src/components/math/arithmetic-operands-generators/multiplication-division/single-digit-operands.ts
@@ -1,5 +1,7 @@
 import randomInteger from "../../randoms/get-random-integer-in-a-range";
 
+import { operations } from "../../../../TS/constatnts/constants";
+
 /**
  *
  */
@@ -7,13 +9,26 @@ const singleDigitOperands = (operation: string, numberOfOperands: number) => {
   const operands: number[] = [];
 
   try {
+    if (
+      operation !== operations.multiplication &&
+      operation !== operations.division
+    )
+      throw new Error(
+        `Unsupported operation ${operation} passed to the "Single digit and up to twenty" generator!`
+      );
+
+    if (typeof numberOfOperands !== "number" || numberOfOperands < 2)
+      throw new Error(
+        `Not applicable number of operands ${numberOfOperands} has been passed to the "Single digit and up to twenty" generator!`
+      );
+
     // 1. Generate problem maximum with limits min=0+numberOfOperands, max=10
     let product = 0;
 
     let operand = 0;
 
     // 2. Loop with length <numberOfOperands>-1
-    for (let i = 0; i < 2; i++) {
+    for (let i = 0; i < numberOfOperands; i++) {
       // 3. Generate operand with limits min=0 (or 1), max=sum-problemTotal
       operand = randomInteger(2, 9);
       product = product === 0 ? operand : product * operand;
@@ -24,14 +39,14 @@ const singleDigitOperands = (operation: string, numberOfOperands: number) => {
 
     // 5. Push the problem maximum value to the appropriate place depend on operation (addition/subtraction)
     operands.splice(operation === "×" ? operands.length : 0, 0, product);
-
-    return operands;
   } catch (e) {
     if (e instanceof Error) {
       throw new Error(e.message);
     } else if (typeof e === "string") {
       throw new Error(e);
     }
+  } finally {
+    return operands;
   }
 };
 
