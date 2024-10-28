2ab9274b0e2d31addf05cc90b04bb5b65c780cf8
lx-kish <urban.ktulhu@gmail.com>
2024-10-28 21:48:53 +1300

    fix(core): fix children prop types in collapsible components
    - fix children prop types in collapsible.component.tsx;
    
    - fix children prop types in collapsible-sticky.component.tsx;
    
    - fix children prop types in collapsible-frame.component.tsx;
    
    - rename test suite for collapsible.component.tsx.

--- src/components/views/footer/footer.component.tsx
+++ src/components/views/footer/footer.component.tsx
@@ -6,7 +6,7 @@ import IconLxKish from "../../icons-svg/icon-lx-kish.component";
 function Footer(): ReactElement {
   return (
     <footer className="footer">
-      <p className="footer__version">v11.6.1</p>
+      <p className="footer__version">v11.6.2</p>
       <p className="footer__credentials">
         &#169; 2020 - 2024. Built with
         <IconHeart className="footer__icon red" />
