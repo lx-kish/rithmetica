8f38d77354232ac60d53e425b6359aacb3eb925c
lx-kish <urban.ktulhu@gmail.com>
2024-11-29 21:54:22 +1300

    test(core): adjust unit tests for settings.compound.component.tsx
    - add test suit for Body component.

--- src/components/views/settings/settings.compound.component.test.tsx
+++ src/components/views/settings/settings.compound.component.test.tsx
@@ -2,11 +2,13 @@ import { ReactElement, ReactNode } from "react";
 import { Provider } from "react-redux";
 import { ActionCreatorWithPayload, configureStore } from "@reduxjs/toolkit";
 
-import { render, screen } from "@testing-library/react";
+import { cleanup, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { describe, it, vi, expect } from "vitest";
 
 import Settings from "./settings.compound.component";
 
+import { useAppDispatch } from "../../../redux/hooks";
 import problemsReducer from "../../../redux/problems/problemsSlice";
 import { ISettings } from "../../../TS/interfaces/interfaces";
 import { TRoutes, TUIType } from "../../../TS/types/types";
@@ -77,6 +79,10 @@ const mockSettingsState: ISettings[] = [
   },
 ];
 
+vi.mock("../../../redux/hooks", () => ({
+  useAppDispatch: vi.fn(() => vi.fn()),
+}));
+
 const mockGenerateProblem = vi.fn() as unknown as ActionCreatorWithPayload<
   { route: string },
   "problems/generateProblems"
@@ -137,6 +143,115 @@ describe("Settings compound component test suit", () => {
       );
 
       expect(screen.getByText("test children content")).toBeInTheDocument();
+    });
+  });
+
+  describe("Body component test suit", () => {
+    afterEach(() => {
+      vi.clearAllMocks();
+      cleanup();
+    });
+
+    it("renders the Settings.Body compound component with default props", () => {
+      renderWithProvider(
+        <RenderWithSettingsContext>
+          <Settings.Body>
+            <div>test children content</div>
+          </Settings.Body>
+        </RenderWithSettingsContext>
+      );
+      expect(screen.getByText("test children content")).toBeInTheDocument();
+    });
+
+    it("should render the Collapsible component with the correct title", () => {
+      renderWithProvider(
+        <RenderWithSettingsContext>
+          <Settings.Body>
+            <div>test children content</div>
+          </Settings.Body>
+        </RenderWithSettingsContext>
+      );
+
+      expect(screen.getByRole("heading")).toHaveTextContent("Settings");
+    });
+
+    it("should render the children inside the Collapsible component", () => {
+      renderWithProvider(
+        <RenderWithSettingsContext>
+          <Settings.Body>
+            <div>test children content</div>
+          </Settings.Body>
+        </RenderWithSettingsContext>
+      );
+      expect(screen.getByText("test children content")).toBeInTheDocument();
+    });
+
+    it("should render the Generate button", () => {
+      renderWithProvider(
+        <RenderWithSettingsContext>
+          <Settings.Body>
+            <div>test children content</div>
+          </Settings.Body>
+        </RenderWithSettingsContext>
+      );
+      expect(
+        screen.getByRole("button", { name: "Generate" })
+      ).toBeInTheDocument();
+    });
+
+    it("should render the expand/collapse button", () => {
+      const { container } = renderWithProvider(
+        <RenderWithSettingsContext>
+          <Settings.Body>
+            <div>test children content</div>
+          </Settings.Body>
+        </RenderWithSettingsContext>
+      );
+
+      expect(container.querySelector(".collapsible__btn")).toBeInTheDocument();
+    });
+
+    it("should apply the correct classes to the root element", () => {
+      renderWithProvider(
+        <RenderWithSettingsContext>
+          <Settings.Body>
+            <div>test children content</div>
+          </Settings.Body>
+        </RenderWithSettingsContext>
+      );
+      expect(screen.getByText("Generate").closest("div")).toHaveClass(
+        "settings"
+      );
+    });
+
+    it("should call handleGenerate when the Generate button is clicked", async () => {
+      const mockDispatch = vi.fn();
+      vi.mocked(useAppDispatch).mockReturnValue(mockDispatch);
+
+      renderWithProvider(
+        <RenderWithSettingsContext>
+          <Settings.Body>
+            <div>test children content</div>
+          </Settings.Body>
+        </RenderWithSettingsContext>
+      );
+      const button = screen.getByText("Generate");
+      await userEvent.click(button);
+      expect(mockDispatch).toHaveBeenCalledTimes(1);
+    });
+
+    it("should not call handleGenerate if button is not clicked", () => {
+      const mockDispatch = vi.fn();
+      vi.mocked(useAppDispatch).mockReturnValue(mockDispatch);
+
+      renderWithProvider(
+        <RenderWithSettingsContext>
+          <Settings.Body>
+            <div>test children content</div>
+          </Settings.Body>
+        </RenderWithSettingsContext>
+      );
+      expect(mockDispatch).not.toHaveBeenCalled();
     });
   });
 });
