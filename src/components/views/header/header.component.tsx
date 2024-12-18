fad5e068e6963cec7d0c56f99ada712959a5d051
lx-kish <urban.ktulhu@gmail.com>
2024-12-18 21:19:09 +1300

    fix(core): adjust header.component.tsx
    - add empty pageName props option;
    
    - add empty children props option;
    
    - add empty section properties check.

--- src/components/views/header/header.component.tsx
+++ src/components/views/header/header.component.tsx
@@ -3,17 +3,21 @@ import { ReactElement } from "react";
 import sections from "../../../sections";
 
 interface IProps {
-  pageName: string;
-  children: ReactElement;
+  pageName?: string;
+  children?: ReactElement | string | undefined;
 }
 
-function Header({ pageName, children }: IProps): ReactElement {
+function Header({ pageName, children }: IProps): ReactElement | undefined {
   const sectionSettings = sections.find((el) => el.link === pageName);
 
   return (
     <header className="header header--multiplication-tab">
-      <h1 className="header__title">{sectionSettings?.name}</h1>
-      <h3 className="header__title--small">{sectionSettings?.motto}</h3>
+      {sectionSettings && (
+        <h1 className="header__title">{sectionSettings?.name}</h1>
+      )}
+      {sectionSettings && (
+        <h3 className="header__title--small">{sectionSettings?.motto}</h3>
+      )}
       {children}
     </header>
   );
