39228a7e2f7b3c658bf6f3ccbb4b2fa3d6eb4461
lx-kish <urban.ktulhu@gmail.com>
2025-01-05 22:13:08 +1300

    refactor(core): redesign tab-line.component.tsx

--- src/components/views/multiplication-tab/tab-line/tab-line.component.tsx
+++ src/components/views/multiplication-tab/tab-line/tab-line.component.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { Fragment, ReactElement } from "react";
 
 import { useAppSelector } from "../../../../redux/hooks";
 import { subtract } from "../../../../redux/multiplicationTable/multiplicationTabSlice";
@@ -11,53 +11,49 @@ import Sign from "../../sign/sign.component";
 
 interface IProps {
   className: string;
-  subtract: boolean;
-  sign: string;
+  subtract?: boolean;
+  sign?: string;
   value: number;
 }
 
-const TabLine: React.FC<IProps> = (props) => {
+function TabLine({ className, value }: IProps) {
   const subtractState = useAppSelector(subtract);
 
   /**
    * Returns mathematical sign for addition or subtraction
-   * @return {html entity} sign code
+   * @return {ReactElement} sign code
    */
-  const getSign = (): any => {
+  function getSign(): ReactElement {
     return subtractState ? <>&#x2212;</> : <>&#x2b;</>;
-  };
+  }
 
   return (
-    <div className={`tab__line ${props.className}`}>
+    <div className={`tab__line ${className}`}>
       <div
-        className={`tab__heading-cell tab__heading-cell--side ${
-          content.styles[props.value]
-        }`}
+        className={`tab__heading-cell tab__heading-cell--side ${content.styles[value]}`}
       >
-        {props.value}
+        {value}
       </div>
       {[...Array(11)].map((x, i) => {
         let j = subtractState ? 10 - i : i;
-        if (j > 1) {
-          return (
-            <React.Fragment key={j}>
+        return (
+          j > 1 && (
+            <Fragment key={j}>
               <TabEmptyCell className="tab__empty-cell">
                 <Sign>{i >= 1 ? getSign() : ""}</Sign>
               </TabEmptyCell>
-              <TabCell line={props.value} col={j} value={j * props.value} />
-            </React.Fragment>
-          );
-        } else return null;
+              <TabCell line={value} col={j} value={j * value} />
+            </Fragment>
+          )
+        );
       })}
       <div
-        className={`tab__heading-cell tab__heading-cell--side ${
-          content.styles[props.value]
-        }`}
+        className={`tab__heading-cell tab__heading-cell--side ${content.styles[value]}`}
       >
-        {props.value}
+        {value}
       </div>
     </div>
   );
-};
+}
 
 export default TabLine;
