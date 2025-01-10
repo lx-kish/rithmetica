9c1af721ab06f7e97a022db49ee0b0688d95fb88
lx-kish <urban.ktulhu@gmail.com>
2025-01-10 20:52:56 +1300

    refactor(core): redesign icon-burger.component.tsx

--- src/components/views/navigation/burger icon/icon-burger.component.tsx
+++ src/components/views/navigation/burger icon/icon-burger.component.tsx
@@ -1,22 +1,16 @@
-import React from "react";
-
 interface IProps {
   open: boolean;
   setOpen: () => void;
-};
+}
 
-const IconBurger: React.FC<IProps> = ({ open, setOpen }) => {
-
+function IconBurger({ open, setOpen }: IProps) {
   return (
-    <div
-      className={`burger${open ? ' is-active' : ''}`}
-      onClick={setOpen}
-    >
+    <div className={`burger${open ? " is-active" : ""}`} onClick={setOpen}>
       <span className="line" />
       <span className="line" />
       <span className="line" />
     </div>
   );
-};
+}
 
 export default IconBurger;
